e8e38bd489dc1fd41634782fad3df42f0b583bdd
Saurav Sahu <mrsauravsahu@outlook.com>
2021-03-11 00:08:33 +0530

    render cards for each vocab

--- src/routes/home/index.svelte
+++ src/routes/home/index.svelte
@@ -1,3 +1,32 @@
+<script>
+import Card from "../../components/card.svelte";
+
+    const vocabs = [
+        {
+            language: 'fr',
+            word: 'mange',
+            sentences: [
+                {
+                    text: 'je mange de la pizza.'
+                }
+            ]
+        },
+        {
+            language: 'fr',
+            word: 'arrondissement',
+            sentences: [
+                {
+                    text: 'je suis à la deuxième arrondissement.'
+                }
+            ]
+        }
+    ]
+</script>
+
 <section>
     <h1>welcome back</h1>
+
+    {#each vocabs as vocab}
+        <Card />
+    {/each}
 </section>
