d6b091a76efb604eb0126844f346c6d1df59283f
Saurav Sahu <mrsauravsahu@outlook.com>
2021-03-11 00:13:21 +0530

    add card style and add a slot

--- src/components/card.svelte
+++ src/components/card.svelte
@@ -1,3 +1,13 @@
-<div>
-    card lel
+<div class="container">
+    <slot />
 </div>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        padding: 1rem;
+        background-color: var(--bg-color);
+        border-radius: 1rem;
+    }
+</style>
