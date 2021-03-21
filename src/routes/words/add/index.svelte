93883314316443a7003abf7faff39402576fbf01
Saurav Sahu <mrsauravsahu@outlook.com>
2021-03-21 20:54:13 +0530

    improv: add word page - lower case all fields

--- src/routes/words/add/index.svelte
+++ src/routes/words/add/index.svelte
@@ -1,16 +1,18 @@
 <script>
   import { faCheck } from '@fortawesome/free-solid-svg-icons';
-import IconButton from "../../../components/icon-button.svelte";
+  import IconButton from '../../../components/icon-button.svelte';
 
+  let word = ''
+  let usage = ''
 </script>
 <section class="container">
   <h1>add a new word</h1>
 
   <form>
     <div>what's the word?</div>
-    <input type='text'/>
+    <input type='text' bind:value={word} on:input={() => word = word.toLowerCase()}/>
     <div>let's use it in a sentence</div>
-    <input type='text'/>
+    <input type='text' bind:value={usage} on:input={() => usage = usage.toLowerCase()} />
   </form>
 
   <IconButton icon={faCheck}/>
