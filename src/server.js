23dc9d1f2dca1aa2f411ee39964f5fe7ba1dabcb
Saurav Sahu <mrsauravsahu@outlook.com>
2021-04-02 19:43:15 +0530

    fix: deploy with app name basepath

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
-import sirv from 'sirv';
-import polka from 'polka';
-import compression from 'compression';
 import * as sapper from '@sapper/server';
+import compression from 'compression';
+import polka from 'polka';
+import sirv from 'sirv';
 
-const { PORT, NODE_ENV } = process.env;
+const { PORT, NODE_ENV, DV_APP_NAME } = process.env;
 const dev = NODE_ENV === 'development';
 
+const url = dev ? '/' : DV_APP_NAME;
+	
 polka() // You can also use Express
 	.use(
+		url,
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
