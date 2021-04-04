39f725cecb647938bf067c470daa1637ef7ebfb2
Saurav Sahu <mrsauravsahu@outlook.com>
2021-04-04 11:29:56 +0530

    feat: add a sample story for IconButton

---
+++ src/components/icon-button.stories.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { faPlus } from "@fortawesome/free-solid-svg-icons";
+  import { Meta,Story,Template } from '@storybook/addon-svelte-csf';
+  import IconButton from './icon-button.svelte';
+
+</script>
+
+<Meta
+  title="Components/IconButton"
+  component={IconButton}
+/>
+
+<Template let:args>
+  <IconButton icon={faPlus}/>
+</Template>
+
+<Story name="Add" />
