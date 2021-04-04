0996ff244636ba15ff517e2036935127694ec80a
Saurav Sahu <mrsauravsahu@outlook.com>
2021-04-04 12:38:00 +0530

    improv: use select in IconButton story

--- src/components/icon-button.stories.svelte
+++ src/components/icon-button.stories.svelte
@@ -2,21 +2,24 @@
   import { Meta,Story,Template } from '@storybook/addon-svelte-csf';
   import IconButton from './icon-button.svelte';
 
-
-
-
 </script>
 
 <Meta
   title="Components/IconButton"
   component={IconButton}
   argTypes={{
-    icon: { control: 'text' }
+    icon: { 
+      control: {
+        type: 'select',
+        options: ['add', 'warn', 'tick']
+      }
+    },
+    onClick: { action: 'onClick'}
   }}
 />
 
 <Template let:args>
-  <IconButton icon={args.icon}/>
+  <IconButton icon={args.icon} on:click={args.onClick} />
 </Template>
 
-<Story name="Add" args={{icon: 'add'}}/>
+<Story name="Main" args={{icon: 'add'}} />
