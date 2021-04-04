e6da4dccd9ef138e6271769f5e7217d530e0e140
Saurav Sahu <mrsauravsahu@outlook.com>
2021-04-04 15:07:05 +0530

    feat: add info component for validation messages

---
+++ src/components/info.stories.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { Meta,Story,Template } from '@storybook/addon-svelte-csf';
+  import Info from './info.svelte';
+
+
+
+</script>
+
+<Meta
+  title="Components/Info"
+  component={Info}
+/>
+
+<Template let:args>
+  <Info
+    message={args.message}
+    type={args.type}
+    icon={args.icon}
+    />
+</Template>
+
+<Story name="Warning" args={{
+    type: "warn",
+    message: "word is required field",
+    icon: 'warn--light'
+  }}
+/> 
+
+<Story name="NoIssues" args={{
+    type: "success",
+    message: "no issues so far",
+    icon: 'tick--light'
+  }}
+/> 
---
+++ src/components/info.svelte
@@ -0,0 +1,39 @@
+<script>
+  import IconButton from './icon-button.svelte';
+
+
+
+  export let type;
+  export let icon
+  export let message;
+</script>
+
+<div class={`container ${type}`}>
+  <IconButton {icon}/>
+  <div class='message'>{message}</div>
+</div>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    border-radius: 1.5rem;
+    background-color: var(--accent-color);
+  }
+
+  .message {
+    margin: 0.25rem 0.5rem;
+    color: black;
+    font-weight: 800;
+  }
+
+  .warn {
+    background-color: #F3CD69;
+  }
+
+  .success {
+    background-color: #4AAA5F
+  }
+</style>
