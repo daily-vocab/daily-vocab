98285cbb982bf3ea5c58946511350fb074541866
Saurav Sahu <mrsauravsahu@outlook.com>
2021-04-04 13:22:51 +0530

    fix: test when validation should pass

--- src/utils/word-validator.js
+++ src/utils/word-validator.js
@@ -2,14 +2,17 @@ import * as yup from 'yup';
 
 const wordSchema = yup
   .string()
-  .required('this is a required field')
+  .required('word is a required field')
 
 const usageSchema = yup
   .string()
-  .required('this is a required field')
-  .matches(new RegExp(`${yup.ref('word')}`), {
-    excludeEmptyString: true,
-    message:'the sentence does not contain the specified word'
+  .required('usage is a required field')
+  .test('contains',
+    'the usage does not contain the specified word',
+    (usage, context) => {
+      const { word } = context.parent
+      if (usage && word) return usage.includes(word)
+      return true;
   })
 
 export const vocabSchema = yup.object({
--- src/utils/word-validator.test.js
+++ src/utils/word-validator.test.js
@@ -5,20 +5,32 @@ describe('vocabSchema validator', () => {
     test('if word is missing', () => {
       expect(vocabSchema.validate({
         usage: 'j\'apprends le français'
-      })).rejects.toThrowError('this is a required field')
+      })).rejects.toThrowError('word is a required field')
     })
   
     test('if usage is missing', () => {
       expect(vocabSchema.validate({
         word: 'apprends'
-      })).rejects.toThrowError('this is a required field')
+      })).rejects.toThrowError('usage is a required field')
     })
 
     test('if usage does not contain the word', () => {
       expect(vocabSchema.validate({
         word: 'apprends',
         usage: 'je suis indien'
-      }, { abortEarly: false })).rejects.toThrowError('the sentence does not contain the specified word')
+      }, { abortEarly: false })).rejects.toThrowError('the usage does not contain the specified word')
+    })
+  })
+
+  describe('should pass', () => {
+    test('if usage contains the word', () => {
+      const inputVocab = {
+        word: 'apprends',
+        usage: 'j\'apprends le français'
+      }
+
+      expect(vocabSchema.validate(inputVocab, { abortEarly: false }))
+        .resolves.toEqual(inputVocab)
     })
   })
 })
