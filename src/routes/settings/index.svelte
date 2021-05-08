46a254cfc4488ecb85187b03e9b67d75111b1be3
Saurav Sahu <mrsauravsahu@outlook.com>
2021-05-08 11:47:00 +0530

    add clear vocab setting

--- src/routes/settings/index.svelte
+++ src/routes/settings/index.svelte
@@ -1,3 +1,19 @@
 <section>
   <h1>settings</h1>
+
+  <div class="setting">
+    <p>clear all vocabs</p>
+    <img src="icons/delete--color.svg" alt="clear all vocab icon button" />
+  </div>
 </section>
+
+<style>
+  .setting {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .setting > img {
+    cursor: pointer;
+  }
+</style>
