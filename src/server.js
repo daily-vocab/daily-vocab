5ead8cb7f4f657b54306f6d3594abb2a35f6d928
Saurav Sahu <mrsauravsahu@outlook.com>
2021-05-09 15:29:53 +0530

    use mkcert files to run https locally

--- src/server.js
+++ src/server.js
@@ -2,13 +2,15 @@ import * as sapper from '@sapper/server';
 import compression from 'compression';
 import polka from 'polka';
 import sirv from 'sirv';
+import { readFileSync } from 'fs';
 
-const { PORT, NODE_ENV, DV_APP_NAME } = process.env;
+import { createServer } from 'https';
+const { PORT, NODE_ENV, DV_APP_NAME, DV_CERT_PATH, DV_CERT_KEY_PATH } = process.env;
 const dev = NODE_ENV === 'development';
 
 const url = DV_APP_NAME;
 	
-polka() // You can also use Express
+const {handler} = polka() // You can also use Express
 	.use(
 		url,
 		compression({ threshold: 0 }),
@@ -18,3 +20,14 @@ polka() // You can also use Express
 	.listen(PORT, err => {
 		if (err) console.log('error', err);
 	});
+
+const options = {
+		key: readFileSync(DV_CERT_KEY_PATH),
+		cert: readFileSync(DV_CERT_PATH)
+	}
+
+// Mount Polka to HTTPS server
+const sslPort = 443
+createServer(options, handler).listen(sslPort, _ => {
+    console.log(`> Running on https://localhost:${sslPort}`);
+});
