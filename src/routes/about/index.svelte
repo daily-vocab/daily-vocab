6e4324cefc5c763b07c8667d5d8b696dcbc99db6
Saurav Sahu <mrsauravsahu@outlook.com>
2021-05-12 18:07:18 +0530

    add a basic about page

---
+++ src/routes/about/index.svelte
@@ -0,0 +1,34 @@
+<section>
+  <h1>about daily-vocab</h1>
+  <!-- TODO: Move this to a string store -->
+  <p>
+    Hey! Welcome to daily-vocab. Created (still a work in progress though) to
+    help with my French Learning Journey. The basic idea is this - create a
+    place where you can add new words you're learning in your target language,
+    along with how you can use it in a sentence; and maybe some notes as well.
+  </p>
+
+  <p>
+    Once you've added a certain amount of words, the app can present you with
+    game (a matching sort of a game) to help you remember these words. This,
+    improving your vocabulary.
+  </p>
+
+  <p>
+    The daily part comes in - to help you make this a daily habit by reminding
+    you to add a new word everyday.
+  </p>
+  <p>
+    Would love to hear your thoughts - <a
+      href="https://twitter.com/mrsauravsahu">Saurav</a
+    >
+  </p>
+</section>
+
+<style>
+  a {
+    color: var(--accent-color);
+    font-weight: 800;
+  }
+
+</style>
