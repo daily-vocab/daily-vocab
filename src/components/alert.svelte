ea079e123ce6a4c3b143c5ea03147163fe1ad05b
Saurav Sahu <mrsauravsahu@outlook.com>
2021-05-15 20:58:25 +0530

    add custom confirmation for clear all vocabs

---
+++ src/components/alert.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { fade } from "svelte/transition";
+
+</script>
+
+<div class="cmp-alert" in:fade out:fade>
+  <div class="alert-window">
+    <slot />
+  </div>
+</div>
+
+<style>
+  .cmp-alert {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    backdrop-filter: blur(0.25rem);
+    position: fixed;
+    height: 100vh;
+    width: 100vw;
+    top: 0;
+    left: 0;
+  }
+
+  .alert-window {
+    height: auto;
+    margin: 1rem;
+    padding: 1rem;
+    border-radius: 1rem;
+    background-color: #b1d0ff55;
+    box-shadow: 0.25rem 0.25rem 1rem rgb(194, 194, 194);
+  }
+
+</style>
